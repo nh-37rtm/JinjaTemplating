9321aa63dc2eb779fd6283ac5e54af5f6b8111d8
nheim <nicolas.heim@acedigitale.com>
2022-05-19 14:17:09 +0200

    multi documents + validators

--- Modules/Parser/YamlParser.py
+++ Modules/Parser/YamlParser.py
@@ -1,8 +1,14 @@
 import yaml
 
-def ImportYamlFile(filePath : str):
+def ImportYamlFile(filePath : str, multipleDocsAllowed : bool = False):
     with open(filePath, 'r', encoding='utf-8') as file:
-        result = yaml.safe_load(file)
+        result = None
+        if ( multipleDocsAllowed ):
+            result = list(yaml.safe_load_all(file))
+            if result is None:
+                raise Exception("no documents found in the generated yaml file !")
+        else:
+            result = yaml.safe_load(file)
         return result
 
 
